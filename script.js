eb3a4000e1b5f0844e67b7b76c4cbb9218c09e8d
Mohamed Wasim Mohamed Akram <wasim.mohamed@ug.bilkent.edu.tr>
2022-09-05 03:03:56 +0300

    Final Etch a Sketch

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const mainDiv = document.querySelector('.mainDiv');
-for(let i = 0; i < 16; i++){
-    for(let i = 0; i < 16; i++){
+let gridNumber = prompt("Number of squares in a row?");
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+for(let i = 0; i < gridNumber; i++){
+    for(let i = 0; i < gridNumber; i++){
         var childDiv = document.createElement('div');
         childDiv.classList.add('childDiv');
-        childDiv.setAttribute('style', 'flex-basis: 6%; border: 1px solid black');
-        let randColor = generateRandomColor();
-        childDiv.setAttribute('onmouseover', 'this.style.background= "red"');
+        childDiv.style.border = "1px solid black"
+        childDiv.style.flexBasis = 100/gridNumber + "%";
+        // childDiv.setAttribute('style', 'flex-basis: 6%');
         mainDiv.appendChild(childDiv);
     }
 }
@@ -15,4 +17,16 @@ function generateRandomColor(){
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
+
+const childDiv2 = document.querySelectorAll('.childDiv');
+childDiv2.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = generateRandomColor();
+    })
+});
+
+const newGridBtn = document.querySelector('.newGridBtn');
+newGridBtn.addEventListener('click', () => {
+    location.reload();
+})
 // childDiv.setAttribute('id', 'childDiv');
